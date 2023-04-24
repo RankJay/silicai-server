bf89bceba12f5d842505ce9dc9c061c6b6bd9560
unknown <rankjay0101@gmail.com>
2023-04-25 00:00:44 +0800

    generating images on server and rate limit system

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { EnvConfig } from './common/config/env';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
+import { FetchModule } from 'nestjs-fetch';
 
 @Module({
-  imports: [HttpModule, EnvConfig],
+  imports: [HttpModule, EnvConfig, FetchModule],
   controllers: [AppController],
   providers: [AppService],
 })
